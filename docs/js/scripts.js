98a5af534f848b0665d8b7810494f2c00fa59e8c
sebswagner <149871314+sebswagner@users.noreply.github.com>
2024-07-14 16:30:44 +0200

    Header animation and section fade in
    Added subtle animation when header items are hovered
    
    Added sections for each section on the page and through JS added a slight fade in animation

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,9 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Handle click to expand education sections
     const educationSections = document.querySelectorAll('.education-section');
-
+    
     educationSections.forEach(section => {
-        section.addEventListener('click', () => {
+        section.querySelector('.education-main').addEventListener('click', () => {
             section.classList.toggle('expanded');
         });
+    });
+
+    // Handle scroll fade-in effect
+    const sections = document.querySelectorAll('.section');
+
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Optional: Stop observing once the animation has been triggered
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
     });
 });
